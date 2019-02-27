9309f0070cf2e5f948f6a4ee4259b6cf6a1b8b7e
Onuk <onishchuk.alexander@gmail.com>
2019-02-27 19:16:49 +0200

    set up entry app.js file

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './components/Header'
+import './sass/main.scss'
+
+ReactDOM.render(<Header />, document.querySelector('#app'))
