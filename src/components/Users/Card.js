ce8abfbb96a4c800cbda5b464be591d23a7b8dbb
Onuk <onishchuk.alexander@gmail.com>
2019-03-03 11:20:09 +0200

    updated Card component for Users to use ellipsize instead of truncate

--- src/components/Users/Card.js
+++ src/components/Users/Card.js
@@ -1,61 +1,43 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Truncate from 'react-truncate'
+import ellipsize from '../../utility/ellipsize'
 
 class Card extends Component {
    constructor(props) {
       super(props)
-      this.card = React.createRef()
-      this.state = {
-         truncateWidth: 1000,
-      }
+
+      this.nameID = 'userName' + this.props.idSuffix
+      this.positionID = 'userPosition' + this.props.idSuffix
    }
 
    componentDidMount() {
-      const truncateWidth =
-         this.card.current.clientWidth -
-         this.card.current.firstElementChild.clientWidth -
-         5
-      this.setState({ truncateWidth })
+      ellipsize(this.nameID)
+      ellipsize(this.positionID)
+   }
+
+   addDefaultSrc = e => {
+      e.target.src = '/images/unknown.jpeg'
    }
    render() {
       return (
          <div className={this.props.className} ref={this.card}>
-            <img src={this.props.photo} alt={this.props.name} />
+            <img
+               onError={this.addDefaultSrc}
+               src={this.props.photo}
+               alt={this.props.name}
+            />
             <div className={this.props.className + '__info'}>
-               <h4 className="heading-4">
-                  <Truncate
-                     lines={5}
-                     width={this.state.truncateWidth}
-                     ellipsis={<span>...</span>}
-                  >
-                     {this.props.name}
-                  </Truncate>
+               <h4 className="heading-4" id={this.nameID}>
+                  {this.props.name}
                </h4>
-               <p className="paragraph-3">
-                  <Truncate
-                     lines={5}
-                     width={this.state.truncateWidth}
-                     ellipsis={<span>...</span>}
-                  >
-                     {this.props.position}
-                  </Truncate>
+               <p className="paragraph-3" id={this.positionID}>
+                  {this.props.position}
                </p>
-               <p className="paragraph-3">
-                  <Truncate
-                     width={this.state.truncateWidth}
-                     ellipsis={<span>...</span>}
-                  >
-                     {this.props.email}
-                  </Truncate>
+               <p className="paragraph-3" title={this.props.email}>
+                  {this.props.email}
                </p>
-               <p className="paragraph-3">
-                  <Truncate
-                     width={this.state.truncateWidth}
-                     ellipsis={<span>...</span>}
-                  >
-                     {this.props.phone}
-                  </Truncate>
+               <p className="paragraph-3" title={this.props.phone}>
+                  {this.props.phone}
                </p>
             </div>
          </div>
@@ -70,6 +52,7 @@ Card.propTypes = {
    position: PropTypes.string,
    email: PropTypes.string,
    phone: PropTypes.string,
+   idSuffix: PropTypes.string,
 }
 
 export default Card
