ab658a76f5fc98fb4420ffe23da61a6c2555fb89
Onuk <onishchuk.alexander@gmail.com>
2019-03-03 11:26:26 +0200

    updated Card for Users with ellipsize for overflow control

--- src/components/Relationships/Card.js
+++ src/components/Relationships/Card.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ellipsize from '../../utility/ellipsize'
 
-const Card = props => {
-   return (
-      <div className={props.className}>
-         <img src={props.image} alt={props.title} />
-         <h3 className="heading-3">{props.title}</h3>
-         <p>{props.content}</p>
-      </div>
-   )
+class Card extends Component {
+   constructor(props) {
+      super(props)
+
+      this.paragraphID = 'relationshipCardParagraph' + this.props.idSuffix
+   }
+   componentDidMount() {
+      ellipsize(this.paragraphID)
+   }
+   render() {
+      return (
+         <div className={this.props.className}>
+            <img src={this.props.image} alt={this.props.title} />
+            <h3 className="heading-3" title={this.props.title}>
+               {this.props.title}
+            </h3>
+            <p id={this.paragraphID}>{this.props.content}</p>
+         </div>
+      )
+   }
 }
 
 Card.propTypes = {
@@ -16,6 +29,7 @@ Card.propTypes = {
    title: PropTypes.string,
    content: PropTypes.string,
    className: PropTypes.string,
+   idSuffix: PropTypes.string,
 }
 
 export default Card
