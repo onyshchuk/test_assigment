1a969bace33ae13c479da10e87a7520e6be7fcf9
Onuk <onishchuk.alexander@gmail.com>
2019-03-03 11:21:05 +0200

    updated Requirements component to use ellipsize fro overflow control

--- src/components/Requirements/index.js
+++ src/components/Requirements/index.js
@@ -1,43 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ellipsize from '../../utility/ellipsize'
 
-const Requirements = () => {
-   return (
-      <section className="section-requirements">
-         <div className="container">
-            <h2 className="heading-2">
-               General requirements for the test task
-            </h2>
-            <div className="requirements__wrapper">
-               <p>
-                  Users want to find answers to their questions quickly and data
-                  shows that people really care about how quickly their pages
-                  load. The Search team announced speed would be a ranking
-                  signal for desktop searches in 2010 and as of this month (July
-                  2018), page speed will be a ranking factor for mobile searches
-                  too. <br />
-                  <br />
-                  If you&apos;re a developer working on a site, now is a good
-                  time to evaluate your performance using our speed tools. Think
-                  about how performance affects the user experience of your
-                  pages and consider measuring a variety of real-world
-                  user-centric performance metrics.
-                  <br />
-                  <br />
-                  Are you shipping too much JavaScript? Too many images? Images
-                  and JavaScript are the most significant contributors to the
-                  page weight that affect page load time based on data from HTTP
-                  Archive and the Chrome User Experience Report - our public
-                  dataset for key UX metrics as experienced by Chrome users
-                  under real-world conditions.
-               </p>
-               <img
-                  src="images/man-laptop-v1.svg"
-                  alt="general requirements for this task"
-               />
+class Requirements extends Component {
+   constructor(props) {
+      super(props)
+
+      this.paragraphID = 'requirementsParagraph'
+   }
+   componentDidMount() {
+      window.setTimeout(() => {
+         ellipsize(this.paragraphID)
+      }, 0)
+   }
+   render() {
+      return (
+         <section className="section-requirements">
+            <div className="container">
+               <h2
+                  className="heading-2"
+                  title="General requirements for the test task"
+               >
+                  General requirements for the test task
+               </h2>
+               <div className="requirements__wrapper">
+                  <p id={this.paragraphID}>
+                     Users want to find answers to their questions quickly and
+                     data shows that people really care about how quickly their
+                     pages load. The Search team announced speed would be a
+                     ranking signal for desktop searches in 2010 and as of this
+                     month (July 2018), page speed will be a ranking factor for
+                     mobile searches too. <br />
+                     <br />
+                     If you&apos;re a developer working on a site, now is a good
+                     time to evaluate your performance using our speed tools.
+                     Think about how performance affects the user experience of
+                     your pages and consider measuring a variety of real-world
+                     user-centric performance metrics.
+                     <br />
+                     <br />
+                     Are you shipping too much JavaScript? Too many images?
+                     Images and JavaScript are the most significant contributors
+                     to the page weight that affect page load time based on data
+                     from HTTP Archive and the Chrome User Experience Report -
+                     our public dataset for key UX metrics as experienced by
+                     Chrome users under real-world conditions.
+                  </p>
+                  <img
+                     src="images/man-laptop-v1.svg"
+                     alt="general requirements for this task"
+                  />
+               </div>
             </div>
-         </div>
-      </section>
-   )
+         </section>
+      )
+   }
 }
 
 export default Requirements
