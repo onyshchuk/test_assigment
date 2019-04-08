7526e2fd5a47151168e790a009a8cdb3c3f202b7
Onuk <onishchuk.alexander@gmail.com>
2019-04-08 19:07:46 +0300

    updated Email with proper styles

--- src/components/SignUp/Email.js
+++ src/components/SignUp/Email.js
@@ -5,14 +5,33 @@ import TextField from '@material-ui/core/TextField'
 import variables from './../../sass/abstracts/_variables.scss'
 
 const styles = () => ({
+   root: {
+      '&$focused $outline': {
+         borderColor: 'grey',
+      },
+   },
    label: {
       fontFamily: variables.fontPrimary,
       transform: 'translate(19px, -6px) scale(0.75) !important',
+
+      '&$focused': {
+         color: 'grey',
+
+         '&$error': {
+            color: '#f44336',
+         },
+      },
    },
    input: {
       fontFamily: variables.fontPrimary,
       fontSize: '1.6rem',
       padding: '18px 18.5px',
+   },
+   focused: {},
+   error: {
+      '&$focused $outline': {
+         borderColor: '#f44336',
+      },
    },
    outline: {
       paddingLeft: '14px !important',
@@ -64,10 +83,15 @@ class Email extends Component {
                shrink: true,
                classes: {
                   shrink: classes.label,
+                  focused: classes.focused,
+                  error: classes.error,
                },
             }}
             InputProps={{
                classes: {
+                  root: classes.root,
+                  focused: classes.focused,
+                  error: classes.error,
                   notchedOutline: classes.outline,
                   input: classes.input,
                },
