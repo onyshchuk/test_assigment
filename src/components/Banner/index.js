438c6e22e07b97e9800a520a2162bd19a431359a
Onuk <onishchuk.alexander@gmail.com>
2019-04-16 18:48:48 +0300

    added text formating according to 2K resolution

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PrimaryButton from '../Buttons/PrimaryButton'
 import ellipsize, { nodeEllipsize } from '../../utility/ellipsize'
+import scrollToElement from '../../utility/scrollToElement'
 
 class Banner extends Component {
    constructor(props) {
@@ -8,12 +9,14 @@ class Banner extends Component {
       this.headerID = 'bannerHeader'
       this.paragraphID = 'bannerParagraph'
       this.buttonID = 'bannerButton'
+      this.screenWidth = 0
    }
    componentDidMount() {
       ellipsize(this.headerID)
       ellipsize(this.paragraphID)
       const button = document.getElementById(this.buttonID)
       nodeEllipsize(button.firstElementChild, button)
+      this.screenWidth = window.screen.width
    }
 
    render() {
@@ -24,16 +27,18 @@ class Banner extends Component {
                   Test assignment for Frontend Developer position
                </h1>
                <p id={this.paragraphID}>
-                  We kindly remind you that your test assignment should be
-                  submitted as a link to github/bitbucket repository. Please be
-                  patient, we consider and respond to every application that
-                  meets minimum requirements. We look forward to your
-                  submission. Good luck!
+                  We kindly remind you that your test assignment should
+                  {this.screenWidth > 1170 && <br />} be submitted as a link to
+                  github/bitbucket repository.
+                  {this.screenWidth > 1170 && <br />} Please be patient, we
+                  consider and respond to every application that meets minimum
+                  requirements. We look forward to your submission. Good luck!
                </p>
                <PrimaryButton
                   variant="contained"
                   className="Button__primary"
                   id="bannerButton"
+                  onClick={() => scrollToElement('signup')}
                >
                   Sign Up
                </PrimaryButton>
