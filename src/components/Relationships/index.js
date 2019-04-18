8ee0b6cc4d943948206b61010427e3b85a1d0d38
Onuk <onishchuk.alexander@gmail.com>
2019-04-18 18:47:23 +0300

    Updated Relationships with responsiveness

--- src/components/Relationships/index.js
+++ src/components/Relationships/index.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
 import Card from './Card'
 import ellipsize from '../../utility/ellipsize'
+import throttle from 'lodash.throttle'
 
 class Relationships extends Component {
    constructor(props) {
       super(props)
 
+      this.state = {
+         screenWidth: 0
+      }
+
       this.headerID = 'relationshipsHeader'
    }
+
    componentDidMount() {
       ellipsize(this.headerID)
+      this.updateScreenWidth()()
+      window.addEventListener('resize', this.updateScreenWidth())
    }
+
+   componentWillUnmount() {
+      window.removeEventListener('resize', this.updateScreenWidth())
+   }
+
+   updateScreenWidth = () => {
+      return throttle(() => {
+         this.setState({ screenWidth: window.screen.width })
+      }, 200)
+   }
+
    render() {
       return (
          <section className="section-relationships">
@@ -31,14 +50,14 @@ class Relationships extends Component {
                      className="relationships__card"
                      image="images/css.svg"
                      title="CSS is my best friend"
-                     content={`Cascading Style Sheets (CSS)\nis a style sheet language used for describing the presentation of a document written in a markup language like HTML.`}
+                     content={`Cascading Style Sheets (CSS) ${((this.state.screenWidth < 1170 && this.state.screenwidth > 900) ? '\n' : '')}is a style sheet language used for describing the presentation of a document written in a markup language like HTML.`}
                   />
                   <Card
                      idSuffix="card-3"
                      className="relationships__card"
                      image="images/javascript.svg"
                      title="JavaScript is my passion"
-                     content="JavaScript is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm."
+                     content={`JavaScript is a high-level, interpreted programming language.${((this.state.screenWidth < 900) ? '\n' : '')} It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.`}
                   />
                </div>
             </div>
