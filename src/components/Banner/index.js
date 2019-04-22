97d60125ced2535dc2c336e5ef1d77a294f21bb6
Onuk <onishchuk.alexander@gmail.com>
2019-04-22 23:14:52 +0300

    Added responsiveness to Banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react'
 import PrimaryButton from '../Buttons/PrimaryButton'
 import ellipsize, { nodeEllipsize } from '../../utility/ellipsize'
 import scrollToElement from '../../utility/scrollToElement'
+import throttle from 'lodash.throttle'
 
 class Banner extends Component {
    constructor(props) {
       super(props)
+
+      this.state = { screenWidth: 0 }
+
       this.headerID = 'bannerHeader'
       this.paragraphID = 'bannerParagraph'
       this.buttonID = 'bannerButton'
-      this.screenWidth = 0
+      this.desktopTwoK = 1200
+      this.mobile = 600
    }
    componentDidMount() {
+      this.updateScreenWidth()()
+      window.addEventListener('resize', this.updateScreenWidth())
       ellipsize(this.headerID)
       ellipsize(this.paragraphID)
       const button = document.getElementById(this.buttonID)
       nodeEllipsize(button.firstElementChild, button)
-      this.screenWidth = window.screen.width
+   }
+
+   componentWillUnmount() {
+      window.removeEventListener('resize', this.updateScreenWidth())
+   }
+
+   updateScreenWidth = () => {
+      return throttle(() => {
+         this.setState({ screenWidth: window.screen.width })
+      }, 200)
    }
 
    render() {
@@ -28,11 +44,11 @@ class Banner extends Component {
                </h1>
                <p id={this.paragraphID}>
                   We kindly remind you that your test assignment should
-                  {this.screenWidth > 1170 && <br />} be submitted as a link to
-                  github/bitbucket repository.
-                  {this.screenWidth > 1170 && <br />} Please be patient, we
-                  consider and respond to every application that meets minimum
-                  requirements. We look forward to your submission. Good luck!
+                  {this.state.screenWidth > this.desktopTwoK && <br />} be
+                  submitted as a link to github/bitbucket repository.
+                  {this.state.screenWidth > this.desktopTwoK && <br />}
+                  {this.screenWidth <= this.mobile &&
+                     'Please be patient, we consider and respond to every application that meets minimum requirements. We look forward to your submission. Good luck!'}
                </p>
                <PrimaryButton
                   variant="contained"
