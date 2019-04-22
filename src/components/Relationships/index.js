ea2ebc96c82f9a75c55a10d9a82d9e312bd0e8e2
Onuk <onishchuk.alexander@gmail.com>
2019-04-22 23:16:20 +0300

    Added responsiveness to Relationships component

--- src/components/Relationships/index.js
+++ src/components/Relationships/index.js
@@ -7,11 +7,13 @@ class Relationships extends Component {
    constructor(props) {
       super(props)
 
-      this.state = {
-         screenWidth: 0
-      }
+      this.state = { screenWidth: 0 }
 
       this.headerID = 'relationshipsHeader'
+      this.desktopTwoK = 1200
+      this.tablet = 900
+      this.tabletMin = 710
+      this.mobile = 600
    }
 
    componentDidMount() {
@@ -43,21 +45,36 @@ class Relationships extends Component {
                      className="relationships__card"
                      image="images/html.svg"
                      title="I'm in love with HTML"
-                     content="Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications."
+                     content={`Hypertext Markup Language (HTML) ${
+                        this.state.screenWidth <= this.mobile ? '\n' : ''
+                     }is the standard markup language for creating web pages and web applications.`}
+                     screenWidth={this.state.screenWidth}
                   />
                   <Card
                      idSuffix="card-2"
                      className="relationships__card"
                      image="images/css.svg"
                      title="CSS is my best friend"
-                     content={`Cascading Style Sheets (CSS) ${((this.state.screenWidth < 1170 && this.state.screenwidth > 900) ? '\n' : '')}is a style sheet language used for describing the presentation of a document written in a markup language like HTML.`}
+                     content={`Cascading Style Sheets (CSS) ${
+                        this.state.screenWidth < this.desktopTwoK &&
+                        this.state.screenwidth > this.tablet
+                           ? '\n'
+                           : ''
+                     }is a style sheet language used for describing the presentation of a document written in a markup language like HTML.`}
+                     screenWidth={this.state.screenWidth}
                   />
                   <Card
                      idSuffix="card-3"
                      className="relationships__card"
                      image="images/javascript.svg"
                      title="JavaScript is my passion"
-                     content={`JavaScript is a high-level, interpreted programming language.${((this.state.screenWidth < 900) ? '\n' : '')} It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.`}
+                     content={`JavaScript is a high-level, interpreted programming language.${
+                        this.state.screenWidth < this.tablet &&
+                        this.state.screenWidth > this.tabletMin
+                           ? '\n'
+                           : ''
+                     } It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.`}
+                     screenWidth={this.state.screenWidth}
                   />
                </div>
             </div>
