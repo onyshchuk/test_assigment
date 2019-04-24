2cccf142338fea0e5bcfd29eea733dc3d377a557
Onuk <onishchuk.alexander@gmail.com>
2019-04-24 14:08:54 +0300

    added production URL

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const request = axios.create({
-   baseURL: '/api/v1/',
+   // baseURL: '/api/v1/',
+   baseURL: 'https://frontend-test-assignment-api.abz.agency/api/v1/',
 })
 
 export default request
