4b5e92a8f4f0fe6efaef2c942438b65fe63c43ce
Onuk <onishchuk.alexander@gmail.com>
2019-04-24 14:07:50 +0300

    updated ellipsize. see comment

--- src/utility/ellipsize.js
+++ src/utility/ellipsize.js
@@ -10,9 +10,10 @@ export const nodeEllipsize = (node, ref) => {
    // ref - ellipsized block wrapper
    // if ref is not specified - block has no wraper - use itself as ref
    if (!ref) ref = node
-   // console.log(node.scrollHeight, ref.offsetHeight)
 
-   while (node.scrollHeight > ref.offsetHeight) {
+   // for some reason sometimes  offsetHeight is smaller then scrollHeight
+   // for some periods of time
+   while (node.scrollHeight - ref.offsetHeight > 3) {
       node.innerHTML = string.substr(0, stringLength) + '...'
       stringLength--
    }
