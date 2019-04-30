5700ccd002ce179397e63b98149a057caf003a19
Onuk <onishchuk.alexander@gmail.com>
2019-04-30 15:59:15 +0300

    added ellipsize to Modal

--- src/components/SignUp/Modal.js
+++ src/components/SignUp/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import TextButton from './../Buttons/TextButton'
@@ -8,6 +8,7 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import variables from './../../sass/abstracts/_variables.scss'
+import ellipsize, { nodeEllipsize } from '../../utility/ellipsize'
 
 const styles = () => ({
    dialog: {
@@ -27,46 +28,69 @@ const styles = () => ({
       letterSpacing: '0.3px',
       lineHeight: 1.4,
       width: '90%',
+      maxHeight: '45px',
    },
    button: {
       margin: '8px 1px 17px 4px',
    },
 })
 
-const Modal = props => {
-   const { dialog, title, text, button } = props.classes
-   return (
-      <div>
-         <Dialog
-            open={props.open}
-            onClose={props.handleModalClose}
-            classes={{ paper: dialog }}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-         >
-            <DialogTitle
-               id="alert-dialog-title"
-               disableTypography
-               classes={{ root: title }}
+class Modal extends Component {
+   constructor(props) {
+      super(props)
+
+      this.buttonID = 'modalButtonID'
+   }
+
+   componentDidMount() {
+      window.setTimeout(() => {
+         ellipsize('alert-dialog-description')
+         const button = document.getElementById(this.buttonID)
+         nodeEllipsize(button.firstElementChild, button)
+      }, 0)
+   }
+
+   render() {
+      const { dialog, title, text, button } = this.props.classes
+      return (
+         <div>
+            <Dialog
+               open={this.props.open}
+               onClose={this.props.handleModalClose}
+               classes={{ paper: dialog }}
+               aria-labelledby="alert-dialog-title"
+               aria-describedby="alert-dialog-description"
             >
-               {'Congratulations'}
-            </DialogTitle>
-            <DialogContent>
-               <DialogContentText
-                  id="alert-dialog-description"
-                  classes={{ root: text }}
+               <DialogTitle
+                  id="alert-dialog-title"
+                  disableTypography
+                  className="modal__title"
+                  classes={{ root: title }}
                >
-                  You have succesfully passed the registration
-               </DialogContentText>
-            </DialogContent>
-            <DialogActions className={button}>
-               <TextButton onClick={props.handleModalClose} autoFocus>
-                  OK
-               </TextButton>
-            </DialogActions>
-         </Dialog>
-      </div>
-   )
+                  Congratulations
+               </DialogTitle>
+               <DialogContent>
+                  <DialogContentText
+                     id="alert-dialog-description"
+                     classes={{ root: text }}
+                  >
+                     You have succesfully passed the registration
+                  </DialogContentText>
+               </DialogContent>
+               <DialogActions className={button}>
+                  <TextButton
+                     id={this.buttonID}
+                     className="modal__button"
+                     onClick={this.props.handleModalClose}
+                     autoFocus
+                  >
+                     OK
+                  </TextButton>
+               </DialogActions>
+            </Dialog>
+         </div>
+      )
+   }
 }
 
 Modal.propTypes = {
